4ad81caa0e5a2fc8c12e0472844ed26e7cd27408
siddacious <nospam187+github@gmail.com>
2020-05-07 16:58:46 -0700

    actionified and formatted

--- Adafruit_ZeroPDMSPI.h
+++ Adafruit_ZeroPDMSPI.h
@@ -12,8 +12,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -27,37 +27,38 @@
 
 #if defined(__SAMD51__)
 
+#include "wiring_private.h"
 #include <Arduino.h>
 #include <SPI.h>
-#include "wiring_private.h"
 
 class Adafruit_ZeroPDMSPI {
 public:
   // Create a new instance of an PDM audio transmitter over SPI
-  Adafruit_ZeroPDMSPI(SPIClass *theSPI); 
+  Adafruit_ZeroPDMSPI(SPIClass *theSPI);
 
   // Initialize the SPI audio receiver.
   bool begin(uint32_t freq);
 
-  bool decimateFilterWord(uint16_t *value, bool removeDC=true); 
+  bool decimateFilterWord(uint16_t *value, bool removeDC = true);
 
-  void setMicGain(float g=1.0);
+  void setMicGain(float g = 1.0);
 
   float sampleRate;
+
 private:
   SPIClass *_spi = NULL;
   Sercom *_sercom = NULL;
   IRQn_Type _irq;
   volatile uint32_t *_dataReg;
 
-  uint16_t       dcCounter        = 0;     // Rolls over every DC_PERIOD samples
-  uint32_t       dcSum            = 0;     // Accumulates DC_PERIOD samples
-  uint16_t       dcOffsetPrior    = 32768; // DC offset interpolates linearly
-  uint16_t       dcOffsetNext     = 32768; // between these two values
+  uint16_t dcCounter = 0;         // Rolls over every DC_PERIOD samples
+  uint32_t dcSum = 0;             // Accumulates DC_PERIOD samples
+  uint16_t dcOffsetPrior = 32768; // DC offset interpolates linearly
+  uint16_t dcOffsetNext = 32768;  // between these two values
 
-  uint16_t       micGain          = 256;   // 1:1
+  uint16_t micGain = 256; // 1:1
 };
 
-#endif //defined(__SAMD51__)
+#endif // defined(__SAMD51__)
 
 #endif
